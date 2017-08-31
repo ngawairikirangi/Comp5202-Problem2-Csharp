e0c52ac90d772240a32fe773cd841dd9904216bd
Ngawai Rikirangi <9978212@BCS.NET.NZ>
2017-09-01 11:29:19 +1200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Comp5202_Problem2_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
