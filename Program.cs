77e863b046ab0d53499c58a5be4f3f344c8feeb9
Ngawai Rikirangi <9978212@BCS.NET.NZ>
2017-09-04 10:51:54 +1200

    problem2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace Comp5202_Problem2_Csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("===========================================");
+            Console.WriteLine("Display division tables of 1 to 12");
+            Console.WriteLine("===========================================");
+
+            int counter = 12;
+
+            Console.WriteLine("Please enter a number");
+            n=Convert.ToIn32(Console.ReadLine());
+            
+            for(j=1;j<12;j++)
+            {
+            Console.Write("{0}/{1}={2}")
+            }
+
         }
     }
 }
